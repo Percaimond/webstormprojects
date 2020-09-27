0cc51d74c0cfc4a5495e244aec9b2867ea92505d
Daniel <danielweissenborn@outlook.de>
2020-09-27 16:49:40 +0200

    Added Authentification

---
+++ PersonalWebsite/src/components/Index.vue
@@ -0,0 +1,90 @@
+
+
+<template>
+
+  <div>
+
+  <title>Startsite</title>
+  <link rel="stylesheet" href="componentsStyle/styleStartsite.css">
+
+<body>
+<header>
+
+  <div id="top">
+    <h1>Daniel Weißenborn</h1>
+    <h2>
+      Software-Engineer Securityanalyst
+    </h2>
+  </div>
+</header>
+<div>
+  okokko
+</div>
+<section>
+  <div class="row">
+    <div class="column">  <nav>
+      <ul>
+        <li> <a class="link" href="https://github.com/">   Projects </a> </li>
+        <li> <a class="link" href="https://www.ebay.de/sch/daniel_weissenborn/m.html?_nkw=&_armrs=1&_ipg=&_from=">Shop </a></li>
+        <li> <a class="link" href="https://naruto-bleach-and-sonic.fandom.com/wiki/Sonic_the_Hedgehog/Quotes">Advice of Life </a></li>
+      </ul>
+
+      <div>
+        <img rel="preload" id="profile" src="../../static/profilpicture.jpg">
+      </div>
+
+      <div>
+        这就是我
+      </div>
+      <div id="talents">
+        Talente:
+        <ul>
+          <li>Playing piano</li>
+          <li> Speaking: German, English, French, Russian, Japanese, Chinese</li>
+          <li> Skateboarding</li>
+          <li>Pentesting</li>
+
+        </ul>
+      </div>
+
+    </nav>
+    </div>
+    <div class="column">
+      <article>
+        <h4>
+          My Personal Website
+        </h4>
+
+        <span>
+      Welcome to my personal Website where I wrote something about my self.
+    </span>
+      </article></div>
+    <div rel="preload" class="column">
+      <div class="wrapper">
+        <div rel="preload" class="one"> <iframe width="100%" height="100%" frameBorder="0" src="https://www.youtube.com/embed/w4s6H4ku6ZY"></iframe></div>
+        <div class="two"> <iframe width="100%" height="100%" frameBorder="0" src="https://www.youtube.com/embed/Ekqu_Ku7nuY"></iframe></div>
+        <div class="three"> <iframe width="100%" height="100%" frameBorder="0" src="https://www.youtube.com/embed/bbEoRnaOIbs"></iframe></div>
+        <div class="five"> <iframe width="100%" height="100%" frameBorder="0" src="https://www.youtube.com/embed/_Wr7cr6ldy0"></iframe></div>
+        <div class="six"><iframe width="100%" height="100%" frameBorder="0" src="https://www.youtube.com/embed/EDwb9jOVRtU"></iframe></div>
+        <div class="seven"><iframe width="100%" height="100%"  frameBorder="0" src="https://www.youtube.com/embed/c18441Eh_WE"> </iframe>  </div>
+      </div>
+    </div>
+
+  </div>
+</section>
+
+<footer>
+  <p>동짓달 기나긴 밤을 한 허리를 버혀 내어
+    춘풍 니불 아래 서리서리 넣었다가
+    어론님 오신 날 밤이어든 굽이굽이 펴리라</p>
+</footer>
+</body>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Index"
+  }
+</script>
+
