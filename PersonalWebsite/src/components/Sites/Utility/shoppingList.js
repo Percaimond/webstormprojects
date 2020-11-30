157dc24747443d380ec14aa1a72f52e2318a02c5
danielweissenborn@outlook.de <Fut32V?$rfin>
2020-11-30 18:02:02 +0100

    Added comments

--- PersonalWebsite/src/components/Sites/Utility/shoppingList.js
+++ PersonalWebsite/src/components/Sites/Utility/shoppingList.js
@@ -1,18 +1,18 @@
-function addNewItem() {
-    if (document.getElementById("addBox").value != "") {
+function addNewItem() {//method that adds new checklist items
+    if (document.getElementById("addBox").value != "") {//only adds nonempty strings
         let element = document.getElementById("list");
         let para = document.createElement("li");
-        var check = document.createElement("input")
-        para.id = "itemid"
+        var check = document.createElement("input");
+        para.id = "itemid";
         check.type = "checkbox";
         check.name = "checker";
         check.id = "checkerid";
         check.className = "checkerclass";
         para.innerHTML = document.getElementById("addBox").value;
-        para.appendChild(check)
-        element.appendChild(para)
-        document.getElementById("addBox").value = ''
-        check.addEventListener('click', event => {
+        para.appendChild(check);
+        element.appendChild(para);
+        document.getElementById("addBox").value = '';
+        check.addEventListener('click', event => {//click changes items into line-through
             if(check.parentElement.style.textDecoration != "line-through"){
                 check.parentElement.style.textDecoration = "line-through";
             } else {
